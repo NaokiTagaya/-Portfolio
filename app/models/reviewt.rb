0e0bd7646212759d8b7e395e590b52aca44a5a40
NaokiTagaya <nao.tagaya.k05@gmail.com>
2021-11-25 19:30:10 +0900

    モデル作成

--- app/models/reviewt.rb
+++
@@ -1,2 +0,0 @@
-class Reviewt < ApplicationRecord
-end
--- db/migrate/20211125103002_create_reviews.rb
+++ db/migrate/20211125103002_create_reviews.rb
@@ -1,6 +1,6 @@
-class CreateReviewts < ActiveRecord::Migration[5.2]
+class CreateReviews < ActiveRecord::Migration[5.2]
   def change
-    create_table :reviewts do |t|
+    create_table :reviews do |t|
       t.integer :restaurant_id
       t.integer :user_id
       t.text :comment
