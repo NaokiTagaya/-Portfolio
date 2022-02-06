660c6a0f480ccd093e7fa2c950cb69d7cff53826
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-06 23:32:15 +0900

    検証用モデル削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_153105) do
+ActiveRecord::Schema.define(version: 2022_02_04_091237) do
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin", force: :cascade do |t|
     t.bigint "user_id"
