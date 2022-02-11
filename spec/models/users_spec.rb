21fb03906a4e65d5c0febdcd8e38c94d1a2dab54
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-11 23:23:25 +0900

    rspec修正

--- spec/models/users_spec.rb
+++ spec/models/users_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
+RSpec.describe 'User_model', type: :model do
   let(:user) { create(:user, user_name: "Rspecテスト", sex: "1", age: "32", email: "test@rspec.com", password: "hogehoge1234") }
-  
+
   describe 'userモデル必須チェック' do
     it '必須パラメータを全て入力し、有効である' do
       expect(user).to be_valid
