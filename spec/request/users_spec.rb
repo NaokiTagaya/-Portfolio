174cb8b07721f1907cb6b3d27751edad237c3c8c
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-11 23:27:48 +0900

    いったん記載を削除

--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,17 +1,14 @@
 RSpec.describe 'POST /users', type: :request do
   context 'すべてのパラメータが揃っている場合' do
     it '200 OKを返す' do
-
     end
 
     it '接続に成功する' do
-
     end
   end
 
   context 'メールアドレスが不足している場合' do
     it '400 Bad Requestを返す' do
-
     end
   end
 end
