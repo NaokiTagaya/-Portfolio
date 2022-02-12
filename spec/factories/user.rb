f2cf33d01d5b414c3367fed28f7e4e297cdc6e25
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-12 22:55:45 +0900

    rspecエラー修正

---
+++ spec/factories/user.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :user do
+    user_name { "Rspecテスト" }
+    sex { 1 }
+    age { 32 }
+    email { "test@rspec.com" }
+    password              { "hogehoge1234" }
+    password_confirmation { "hogehoge1234" }
+  end
+end
