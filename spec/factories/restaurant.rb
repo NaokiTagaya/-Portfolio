91e9df0f9c019f6cc1597240280aa7bce9f2fdb6
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-13 18:12:48 +0900

    rspec：restaurantモデルテスト記載

---
+++ spec/factories/restaurant.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :restaurant do
+    restaurant_name { "レストランRspec" }
+    tel { "03-1234-5678" }
+    zipcode { "150-0000" }
+    address { "東京都渋谷区渋谷２丁目" }
+    registered_user_id { 1 }
+  end
+end
+
