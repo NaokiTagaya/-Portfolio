c79394a67ea98034e61466b43855cbeec7a8cef3
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-13 18:35:06 +0900

    rspec：reviewモデルテスト記載

--- spec/factories/review.rb
+++ spec/factories/review.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
-    factory :review do
-      rate { 5 }
-      comment { "Rpsecコメント" }
-    end
+  factory :review do
+    restaurant_id { 1 }
+    user_id { 1 }
+    rate { 5 }
+    comment { "Rpsecコメント" }
   end
+end
