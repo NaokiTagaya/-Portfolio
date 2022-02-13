b11fee3e74aeeb2a0739b06a3901c8b2c7eef884
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-13 16:45:07 +0900

    rspec：userモデルテスト記載

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     sex { 1 }
     age { 32 }
     email { "test@rspec.com" }
-    password              { "hogehoge1234" }
+    password { "hogehoge1234" }
     password_confirmation { "hogehoge1234" }
   end
 end
