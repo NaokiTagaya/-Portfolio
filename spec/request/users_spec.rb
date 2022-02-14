e5cc8c963a66f0297ddf1dfc03c7e5347052a032
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-14 15:11:06 +0900

    rspec：userのrequestテストを記載

--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,6 +1,7 @@
 RSpec.describe 'Users', type: :request, js: true do
   describe 'POST #create' do
     let(:test_user) { FactoryBot.create :test_user }
+    let(:fail_user) { attributes_for(:user, user_name: "") }
 
     context 'すべてのパラメータが揃っている場合' do
       it '200 OKを返すこと' do
@@ -17,6 +18,24 @@ RSpec.describe 'Users', type: :request, js: true do
         expect do
           post user_registration_path, params: { id: test_user }
         end.to change(User, :count).by 1
+      end
+    end
+
+    context 'パラメータが不正な場合' do
+      it '400 Bad Requestを返すこと' do
+        post user_registration_path, params: { id: fail_user }
+        expect(response).to have_http_status(400)
+      end
+
+      it 'ユーザー登録が失敗すること' do
+        expect do
+          post user_registration_path, params: { id: fail_user }
+        end.to_not change(User, :count)
+      end
+
+      it 'エラーが表示されること' do
+        post user_registration_path, params: { id: fail_user }
+        expect(response.body).to include 'prohibited this user from being saved'
       end
     end
   end
