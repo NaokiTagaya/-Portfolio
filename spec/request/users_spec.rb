d07eef212a27b9019fb05feb76ea9cacd7711ab2
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-14 11:15:21 +0900

    rspec：userのrequestテストを記載

--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,14 +1,26 @@
-RSpec.describe 'POST /users', type: :request do
-  context 'すべてのパラメータが揃っている場合' do
-    it '200 OKを返す' do
-    end
+RSpec.describe 'Users', type: :request do
+  describe 'POST #create' do
+    context 'すべてのパラメータが揃っている場合' do
+      it '200 OKを返すこと' do
+        post registration_path, params: { user_name: 'Rspecユーザー', age: 32, sex: 1, email: 'rspec@test.com', password: 'hogohoge1234', password_confirmation: 'hogehoge1234' }
+        expect(response).to have_http_status(200)
+      end
 
-    it '接続に成功する' do
-    end
-  end
+      it '接続に成功すること' do
+        post registration_path, params: { user_name: 'Rspecユーザー', age: 32, sex: 1, email: 'rspec@test.com', password: 'hogohoge1234', password_confirmation: 'hogehoge1234' }
+        expect(response).to be_successful
+      end
 
-  context 'メールアドレスが不足している場合' do
-    it '400 Bad Requestを返す' do
+      it 'ユーザー登録が成功すること' do
+        except do
+          post registration_path, params: { user_name: 'Rspecユーザー', age: 32, sex: 1, email: 'rspec@test.com', password: 'hogohoge1234', password_confirmation: 'hogehoge1234' }
+        end.to change(User, :count).by 1
+      end
+
+      it 'リダイレクトされること' do
+        post registration_path, params: { user_name: 'Rspecユーザー', age: 32, sex: 1, email: 'rspec@test.com', password: 'hogohoge1234', password_confirmation: 'hogehoge1234' }
+        expect(response).to redirect_to users_profile_path
+      end
     end
   end
 end
