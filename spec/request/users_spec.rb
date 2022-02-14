6b3a9d58aa59ff7f9bdbb678e5965cec9a2a0aa4
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-14 17:25:07 +0900

    rspec：userのrequestテストを記載

--- spec/request/users_spec.rb
+++ spec/request/users_spec.rb
@@ -1,4 +1,11 @@
 RSpec.describe 'Users', type: :request, js: true do
+  describe 'GET /users/sign_up' do
+    it '会員登録画面の表示に成功すること' do
+      get new_user_registration_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
   describe 'POST #create' do
     let(:test_user) { FactoryBot.create :test_user }
     let(:fail_user) { attributes_for(:user, user_name: "") }
@@ -48,16 +55,17 @@ RSpec.describe 'Users', type: :request, js: true do
   end
 
   describe 'POST /users/sign_in' do
-    let (:req_params) { { session_form: { email: "rspec@test.com", password: input_pass } } }
+    let(:login_user) { FactoryBot.create :login_user }
+    let(:req_params) { { session_form: { email: "rspeclogin@test.com", password: input_pass } } }
     context '登録されたユーザー' do
-      let (:input_pass) { "hogohoge1234" }
+      let(:input_pass) { "hogohoge1234" }
       it 'ログインに成功すること' do
         post session_path, params: req_params
         expect(response).to redirect_to root_path
       end
     end
     context '未登録のユーザー' do
-      let (:input_pass) { "hogohoge5678" }
+      let(:input_pass) { "hogohoge5678" }
       it 'ログインに失敗すること' do
         post session_path, params: req_params
         expect(response.body).to include '失敗しました。'
