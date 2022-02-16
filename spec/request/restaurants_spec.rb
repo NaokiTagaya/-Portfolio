6ee168c6ea6ca4a946f1dfa5938fcd4e86e2ebef
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-16 15:33:30 +0900

    rspec：restaurantのrequestテストを記載

--- spec/request/restaurants_spec.rb
+++ spec/request/restaurants_spec.rb
@@ -1,4 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request, js: true do
+RSpec.describe 'Restaurants', type: :request, js: true do
+  describe 'GET /restaurants/search' do
+    it '検索一覧画面の表示に成功すること' do
+      get restaurants_search_path
+      expect(response).to have_http_status(200)
+      expect(response).to be_successful
+    end
+  end
 end
