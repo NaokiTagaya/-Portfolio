fb18b8cfae4786fd5ded68ae8727c27f82ee9b9d
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-16 15:47:58 +0900

    rspec：restaurantのrequestテストを記載

--- spec/request/restaurants_spec.rb
+++ spec/request/restaurants_spec.rb
@@ -8,4 +8,33 @@ RSpec.describe 'Restaurants', type: :request, js: true do
       expect(response).to be_successful
     end
   end
+
+  describe 'GET /restaurants/new' do
+    it '店舗登録画面の表示に成功すること' do
+      get new_restaurant_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe 'POST #create' do
+    let(:restaurant) { FactoryBot.create :restaurant }
+    let(:fail_restaurant) { attributes_for(:restaurant, restaurant_name: "") }
+    context 'すべてのパラメータが揃っている場合' do
+      it 'リクエストが成功すること' do
+        post restaurants_path, params: { id: restaurant }
+        expect(response).to have_http_status(200)
+      end
+
+      it '接続に成功すること' do
+        post restaurants_path, params: { id: restaurant }
+        expect(response).to be_successful
+      end
+
+      it '店舗登録が成功すること' do
+        expect do
+          post restaurants_path, params: { id: restaurant }
+        end.to change(Restaurant, :count).by 1
+      end
+    end
+  end
 end
