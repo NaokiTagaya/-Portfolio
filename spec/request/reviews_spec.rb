d56e539d7f5fc6a9aa921d6c4988da0ea69f7f58
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-17 15:20:39 +0900

    rspecエラー修正

--- spec/request/reviews_spec.rb
+++ spec/request/reviews_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe 'Reviews', type: :request, js: true do
       sign_in @user
     end
     it 'レビュー投稿画面の表示に成功すること' do
-      get new_review_path, params: { id: restaurant.id }
+      get new_review_path, params: { id: @restaurant.id }
       expect(response).to have_http_status(200)
     end
   end
