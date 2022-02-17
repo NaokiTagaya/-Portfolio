e8869672d31f3fa1d04c9c18c2b4f852d800dd7b
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-17 18:59:56 +0900

    rspecエラー修正

--- spec/request/reviews_spec.rb
+++ spec/request/reviews_spec.rb
@@ -12,4 +12,18 @@ RSpec.describe 'Reviews', type: :request, js: true do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe 'POST #create' do
+    before do
+      @restaurant = FactoryBot.create(:restaurant)
+      @user = FactoryBot.create(:user)
+      sign_in @user
+    end
+    let(:test_review) { FactoryBot.build(:review) }
+    let!(:param_review) { { restaurant_id: @restaurant.id, user_id: @user.id, commnet: test_review.commnet, rate: test_review.rate } }
+    it 'レビュー投稿画面の表示に成功すること' do
+      post reviews_path, params: { review: param_review }
+      expect(response).to have_http_status(200)
+    end
+  end
 end
