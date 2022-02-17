31602968fe9c4b70a37d65c4204a96786a898a68
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-17 13:50:44 +0900

    rspecエラー修正

--- spec/request/reviews_spec.rb
+++ spec/request/reviews_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Reviews', type: :request, js: true do
   describe 'GET /reviews/new' do
     before do
+      @restaurant = FactoryBot.create(:restaurant)
       @user = FactoryBot.create(:user)
       sign_in @user
     end
