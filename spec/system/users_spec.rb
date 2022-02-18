5c6b242d68774d1aab195d602eaadaedc04cbbc6
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-18 12:53:34 +0900

    rspecエラー修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,13 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe 'User', type: :system do
-  before do
-    @user = FactoryBot.create(:regi_user)
-    sign_in @user
-    visit root_path
-  end
-
-  describe 'ログイン後のトップページ表示' do
+  describe 'トップページ' do
+    before do
+      @user = FactoryBot.create(:regi_user)
+    end
+    
+    context 'ユーザーログイン時' do
+      before do
+        visit new_user_session_path
+        fill_in 'メールアドレス', with: 'jiro_potepan@test.com'
+        fill_in 'パスワード', with: 'potepote1234'
+        click_button 'ログイン'
+      end
+    end
     it 'ログイン後のトップページが表示されている' do
       expect(page).to have_link 'ホーム', href: root_path
     end
