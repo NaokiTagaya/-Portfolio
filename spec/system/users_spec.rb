48a7b1765a84d0db1a5946e786a57c3acf292070
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-18 13:01:24 +0900

    rspecエラー修正

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -9,13 +9,14 @@ RSpec.describe 'User', type: :system do
     context 'ユーザーログイン時' do
       before do
         visit new_user_session_path
-        fill_in 'メールアドレス', with: 'jiro_potepan@test.com'
-        fill_in 'パスワード', with: 'potepote1234'
+        fill_in 'メールアドレス', with: "jiro_potepan@test.com"
+        fill_in 'パスワード', with: "potepote1234"
         click_button 'ログイン'
       end
-    end
-    it 'ログイン後のトップページが表示されている' do
-      expect(page).to have_link 'ホーム', href: root_path
+
+      it 'ログイン後のトップページが表示されている' do
+        expect(page).to have_link 'ホーム', href: root_path
+      end
     end
   end
 end
