8f92e44ca76194cfe097108bbaeb78da21c7c081
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-18 17:14:40 +0900

    rspecエラー修正

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,9 +16,6 @@
 require 'capybara/rspec'
 
 RSpec.configure do |config|
-  config.before(:each, type: :system) do
-    driven_by :selenium_chrome_headless
-  end
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
@@ -98,5 +95,4 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-config.full_backtrace = true
 end
