a9b6f3a31b7f777dc4a88dcdf699dabb08b0b021
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-18 12:21:37 +0900

    rspec:userのsystemテストを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,9 +1,23 @@
 require 'rails_helper'
 
-RSpec.describe 'User', type: :feature do
-  scenario "ユーザー登録を行う" do
-    visit root_path
+RSpec.describe 'User', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+  end
 
-    click_link "新規登録"
+  describe 'ログイン後のトップページ表示' do
+    before do
+      visit new_user_session_path
+      fill_in 'メールアドレス', with: 'jiro_potepan@test.com'
+      fill_in 'パスワード', with: 'potepote1234'
+      click_button 'ログイン'
+    end
+
+    it 'ログイン後のトップページが表示されている' do
+      expect(page).to have_link 'ホーム', href: root_path
+      expect(page).to have_link 'マイページ', href: users_profile_path
+      expect(page).to have_link '店舗登録', href: new_restaurant_path
+      expect(page).to have_link 'ログアウト', href: destroy_user_session_path
+    end
   end
 end
