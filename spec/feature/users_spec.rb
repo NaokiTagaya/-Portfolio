b7473b889382cdc46220d046e5283188e5eefcb7
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-18 18:10:26 +0900

    テスト記載追加とrubocop修正

--- spec/feature/users_spec.rb
+++ spec/feature/users_spec.rb
@@ -4,6 +4,11 @@ RSpec.describe 'User', type: :feature do
   scenario 'ログイン前のトップページ表示' do
     visit root_url
 
+    # コンテンツ表示
+    expect(page).to have_link 'ホーム', href: root_path
+    expect(page).to have_link 'ログイン', href: new_user_session_path
+    expect(page).to have_link '新規登録', href: new_user_registration_path
+    expect(page).to have_link '今すぐ登録', href: new_user_registration_path
   end
 
   scenario 'ログイン後のトップページ表示' do
@@ -20,5 +25,6 @@ RSpec.describe 'User', type: :feature do
     expect(page).to have_link 'マイページ', href: users_profile_path
     expect(page).to have_link '店舗登録', href: new_restaurant_path
     expect(page).to have_link 'ログアウト', href: destroy_user_session_path
+    expect(page).to_not have_link '今すぐ登録', href: new_user_registration_path
   end
 end
