c867cc4cfd380f754c4dc038d7643185f2089255
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-20 15:06:59 +0900

    テスト記載追加

--- spec/feature/restaurants_spec.rb
+++ spec/feature/restaurants_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'Restaurant', type: :feature do
     visit root_url
 
     # 何も入力せずに検索
-    find('input.btn-lg').click
+    find('input.btn-sm').click
 
     # コンテンツ表示
     expect(page).to have_content '検索結果：3件'
@@ -22,5 +22,50 @@ RSpec.describe 'Restaurant', type: :feature do
     expect(page).to have_content '神奈川県横浜市西区みなとみらい２丁目３'
     expect(page).to have_content '群馬レストラン'
     expect(page).to have_content '群馬県伊勢崎市上蓮町６５７'
+  end
+
+  scenario 'エリアを絞って店舗検索' do
+    # トップページ
+    visit root_url
+
+    # 何も入力せずに検索
+    fill_in 'エリア', with: '群馬', match: :first
+    find('input.btn-sm').click
+
+    # コンテンツ表示
+    expect(page).to have_content '検索結果：1件'
+    expect(page).to have_content '群馬レストラン'
+    expect(page).to have_content '群馬県伊勢崎市上蓮町６５７'
+  end
+
+  scenario 'キーワードを絞って店舗検索' do
+    # トップページ
+    visit root_url
+
+    # 何も入力せずに検索
+    fill_in 'キーワード', with: 'Rspec', match: :first
+    find('input.btn-sm').click
+
+    # コンテンツ表示
+    expect(page).to have_content '検索結果：2件'
+    expect(page).to have_content 'レストランRspec'
+    expect(page).to have_content '東京都渋谷区渋谷２丁目'
+    expect(page).to have_content 'Rspecレストラン'
+    expect(page).to have_content '神奈川県横浜市西区みなとみらい２丁目３'
+  end
+
+  scenario 'エリアとキーワード両方入力して店舗検索' do
+    # トップページ
+    visit root_url
+
+    # 何も入力せずに検索
+    fill_in 'エリア', with: '神奈川', match: :first
+    fill_in 'キーワード', with: 'Rspec', match: :first
+    find('input.btn-sm').click
+
+    # コンテンツ表示
+    expect(page).to have_content '検索結果：1件'
+    expect(page).to have_content 'Rspecレストラン'
+    expect(page).to have_content '神奈川県横浜市西区みなとみらい２丁目３'
   end
 end
