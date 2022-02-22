ed6a012ea1eb7336847f9243cf20e64242606e33
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-23 01:05:57 +0900

    rspecエラー修正

--- spec/feature/reviews_spec.rb
+++ spec/feature/reviews_spec.rb
@@ -19,10 +19,21 @@ RSpec.describe 'Review', type: :feature do
     # エリアを入力して検索
     fill_in 'large-area', with: '神奈川'
     find('input#large-search').click
+    find('#detail-button').click
 
     # コンテンツ表示
     expect(page).to have_selector("img[src$='default_icon.png']")
     expect(page).to have_content 'ポテパン次郎'
+    expect(page).to have_selector("img[alt=1]")
+    expect(page).to have_selector("img[alt=2]")
+    expect(page).to have_selector("img[alt=3]")
+    expect(page).to have_selector("img[alt=4]")
+    expect(page).to have_selector("img[alt=5]")
+    expect(page).to have_selector("img[src$='star-on-88233ad1abcd2282b53edb9465a6bef42fd32de319f014e4059353e4fd8a7e0a.png']")
+    expect(page).to have_selector("img[src$='star-on-88233ad1abcd2282b53edb9465a6bef42fd32de319f014e4059353e4fd8a7e0a.png']")
+    expect(page).to have_selector("img[src$='star-on-88233ad1abcd2282b53edb9465a6bef42fd32de319f014e4059353e4fd8a7e0a.png']")
+    expect(page).to have_selector("img[src$='star-on-88233ad1abcd2282b53edb9465a6bef42fd32de319f014e4059353e4fd8a7e0a.png']")
+    expect(page).to have_selector("img[src$='star-off-cf9b07584547d5d561dfac9cdbf7b6a530cb72a1b7a1096411966036c4017d38.png']")
     expect(page).to have_content '4.0'
     expect(page).to have_content '評価は4点です。'
   end
