a40422867522956ed78f3f70fe7fba7e918eb638
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-22 23:46:55 +0900

    テスト記載追加

--- spec/factories/review.rb
+++ spec/factories/review.rb
@@ -3,4 +3,14 @@ FactoryBot.define do
     rate { 5 }
     comment { "Rpsecコメント" }
   end
+
+  factory :four_rate, class: Review do
+    rate { 4 }
+    comment { "評価は4点です。" }
+  end
+
+  factory :three_rate, class: Review do
+    rate { 2.5 }
+    comment { "良くも悪くも2.5点ですね。" }
+  end
 end
---
+++ spec/feature/reviews_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+RSpec.describe 'Review', type: :feature do
+  before do
+    @user = FactoryBot.create(:regi_user)
+    @restaurant1 = FactoryBot.create(:kanagawa_restaurant)
+    @restaurant2 = FactoryBot.create(:gunma_restaurant)
+    @four_rate = FactoryBot.create(:four_rate, restaurant_id: @restaurant1.id, user_id: @user.id)
+    @three_rate = FactoryBot.create(:three_rate, restaurant_id: @restaurant2.id, user_id: @user.id)
+  end
+
+  scenario '店舗のレビュー一覧を表示' do
+    # ログイン処理
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: 'jiro_potepan@test.com'
+    fill_in 'パスワード', with: 'potepote1234'
+    click_button 'ログイン'
+
+    # エリアを入力して検索
+    fill_in 'large-area', with: '神奈川'
+    find('input#large-search').click
+
+    # コンテンツ表示
+    expect(page).to have_selector("img[src$='default_icon.png']")
+    expect(page).to have_content 'ポテパン次郎'
+    expect(page).to have_selector("img[alt="1" src$='star-on-88233ad1abcd2282b53edb9465a6bef42fd32de319f014e4059353e4fd8a7e0a.png']")
+    expect(page).to have_selector("img[alt="2" src$='star-on-88233ad1abcd2282b53edb9465a6bef42fd32de319f014e4059353e4fd8a7e0a.png']")
+    expect(page).to have_selector("img[alt="3" src$='star-on-88233ad1abcd2282b53edb9465a6bef42fd32de319f014e4059353e4fd8a7e0a.png']")
+    expect(page).to have_selector("img[alt="4" src$='star-on-88233ad1abcd2282b53edb9465a6bef42fd32de319f014e4059353e4fd8a7e0a.png']")
+    expect(page).to have_selector("img[alt="5" src$='star-off-cf9b07584547d5d561dfac9cdbf7b6a530cb72a1b7a1096411966036c4017d38.png']")
+    expect(page).to have_content '4.0'
+    expect(page).to have_content '評価は4点です。'
+  end
+
+  scenario 'マイページ内の投稿レビュー一覧を閲表示' do
+    
+  end
+
+  scenario 'レビュー投稿' do
+
+  end
+end
