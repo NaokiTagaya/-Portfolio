0fb05011d481f7299cec255f2aac33324a462477
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-24 22:57:21 +0900

    テスト記載追加

--- spec/feature/favorites_spec.rb
+++ spec/feature/favorites_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe 'Favorite', type: :feature do
+RSpec.describe 'Favorite', type: :feature, js: true do
   before do
     @user = FactoryBot.create(:regi_user)
-    @user1 = FactoryBot.create(:profile_user)
+    @user1 = FactoryBot.create(:login_user)
     @restaurant1 = FactoryBot.create(:kanagawa_restaurant)
     @restaurant2 = FactoryBot.create(:gunma_restaurant)
     @favorite_b1 = FactoryBot.create(:favorite_1, user_id: @user.id, restaurant_id: @restaurant1.id)
@@ -34,8 +34,37 @@ RSpec.describe 'Favorite', type: :feature do
   end
 
   scenario 'お気に入り登録後、マイページ内のお気に入り一覧に反映されているか確認' do
+    # ログイン処理
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: 'rspeclogin@test.com'
+    fill_in 'パスワード', with: 'hogohoge1234'
+    click_button 'ログイン'
+
+    # エリア検索を行い、店舗詳細画面に遷移
+    visit root_url
+    fill_in 'large-area', with: '群馬'
+    find('input#large-search').click
+    find('#detail-button').click
+
+    # お気に入り登録を押下
+    expect do
+      post restaurant_favorites_path(restaurant_id: @restaurant2.id), xhr: true
+      end.to change(Favorite, :count).by(1)
+    end
+
+    # マイページコンテンツ表示
+    click_link 'マイページ'
+    expect(page).to have_selector("img[src$='no_image.jpg']")
+    expect(page).to have_content '群馬レストラン'
+    expect(page).to have_content '〒372-0851 群馬県伊勢崎市上蓮町６５７'
+    expect(page).to have_content @favorite_b1.created_at.strftime("%Y.%m.%d")
+    expect(page).to have_link '店舗詳細', href: restaurant_path(@restaurant2.id)
+    expect(page).to_not have_content 'お気に入り登録した店舗はありません'
   end
 
   scenario 'お気に入り解除後、マイページ内のお気に入り一覧に反映されているか確認' do
+  end
+
+  scenario '自身で登録した店舗はお気に入りボタン非表示' do
   end
 end
