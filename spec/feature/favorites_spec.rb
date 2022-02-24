c137cacdcf0b6ca8620d7f682a1132045bc1d36c
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-24 21:54:33 +0900

    rspecエラー修正

--- spec/feature/favorites_spec.rb
+++ spec/feature/favorites_spec.rb
@@ -24,12 +24,12 @@ RSpec.describe 'Favorite', type: :feature do
     expect(page).to have_selector("img[src$='no_image.jpg']")
     expect(page).to have_content 'Rspecレストラン'
     expect(page).to have_content '〒220-0012 神奈川県横浜市西区みなとみらい２丁目３'
-    expect(page).to have_content @favorite_b1.created_at
+    expect(page).to have_content @favorite_b1.created_at.strftime("%Y.%m.%d")
     expect(page).to have_link '店舗詳細', href: restaurant_path(@restaurant1.id)
     expect(page).to have_selector("img[src$='no_image.jpg']")
     expect(page).to have_content '群馬レストラン'
     expect(page).to have_content '〒372-0851 群馬県伊勢崎市上蓮町６５７'
-    expect(page).to have_content @favorite_b2.created_at
+    expect(page).to have_content @favorite_b2.created_at.strftime("%Y.%m.%d")
     expect(page).to have_link '店舗詳細', href: restaurant_path(@restaurant2.id)
   end
 
