c09d72118ae50e0c7316cc90e8c17b7122e4cf48
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-27 14:39:59 +0900

    rspecエラー修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,6 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
-const webpack = require('webpack')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
