a878ca2dcc2f1e366be54e45d4f52fab13126540
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-02-27 13:06:55 +0900

    rspecエラー修正

--- spec/feature/favorites_spec.rb
+++ spec/feature/favorites_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Favorite', type: :feature do
+RSpec.describe 'Favorite', type: :feature, js: true do
   before do
     @user = FactoryBot.create(:regi_user)
     @user1 = FactoryBot.create(:login_user)
@@ -33,12 +33,23 @@ RSpec.describe 'Favorite', type: :feature do
     expect(page).to have_link '店舗詳細', href: restaurant_path(@restaurant2.id)
   end
 
-  scenario 'お気に入り登録後、マイページ内のお気に入り一覧に反映されているか確認', js: true do
+  scenario 'お気に入り登録後、マイページ内のお気に入り一覧に反映されているか確認' do
     # ログイン処理
     visit new_user_session_path
     fill_in 'メールアドレス', with: 'rspeclogin@test.com'
     fill_in 'パスワード', with: 'hogohoge1234'
     click_button 'ログイン'
+  end
+
+  scenario 'お気に入り解除後、マイページ内のお気に入り一覧に反映されているか確認' do
+    # ログイン処理
+    visit new_user_session_path
+    fill_in 'メールアドレス', with: 'jiro_potepan@test.com'
+    fill_in 'パスワード', with: 'potepote1234'
+    click_button 'ログイン'
+
+    # マイページ遷移
+    click_link 'マイページ'
 
     # エリア検索を行い、店舗詳細画面に遷移
     click_link 'ホーム'
@@ -46,23 +57,14 @@ RSpec.describe 'Favorite', type: :feature do
     find('input#large-search').click
     find('#detail-button').click
 
-    # お気に入り登録を押下
-    find('.far').click
+    # お気に入り解除前
     expect(page).to have_css '.fas'
-    expect(page).to have_content 'お気に入り解除'
-    expect(page).to have_css '.star-count1', text: '2'
+    expect(page).to have_css '.star-count1', text: '1'
 
-    # マイページコンテンツ表示
-    click_link 'マイページ'
-    expect(page).to have_selector("img[src$='no_image.jpg']")
-    expect(page).to have_content '群馬レストラン'
-    expect(page).to have_content '〒372-0851 群馬県伊勢崎市上蓮町６５７'
-    expect(page).to have_content @favorite_b1.created_at.strftime("%Y.%m.%d")
-    expect(page).to have_link '店舗詳細', href: restaurant_path(@restaurant2.id)
-    expect(page).to_not have_content 'お気に入り登録した店舗はありません'
-  end
-
-  scenario 'お気に入り解除後、マイページ内のお気に入り一覧に反映されているか確認' do
+    #お気に入り解除押下後
+    find('.fas').click
+    expect(page).to have_css '.far'
+    expect(page).to have_css '.star-count1', text: '0'
   end
 
   scenario '自身で登録した店舗はお気に入りボタン非表示' do
