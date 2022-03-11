d81a600f64ec6630e6b93cd634308d01c2ec6be3
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-03-11 12:05:00 +0900

    コメント追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :restaurant, foreign_key: 'restaurant_id'
   belongs_to :user
 
+  # バリデーション
   validates :rate, presence: true
   validates :rate, numericality: {
   less_than_or_equal_to: 5,
