c19ad0fb56e4608246cec371e43f2124974c2530
NaokiTagaya <nao.tagaya.k05@gmail.com>
2022-03-14 17:37:16 +0900

    rubocopエラーのため修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -13,7 +13,7 @@ class ImageUploader < CarrierWave::Uploader::Base
         :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
         :aws_secret_access_key => ENV['S3_SECRET_KEY']
       }
-      config.fog_directory     =  ENV['S3_BUCKET']
+      config.fog_directory = ENV['S3_BUCKET']
     end
     storage :fog
   else
